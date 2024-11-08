569da78441a6e63fe74471a6b5a64c411b40388c
Friederish <friederishmartinezramirez@gmail.com>
2024-11-07 21:28:29 -0600

    Validaciones de Datos

--- Producto.cs
+++ Producto.cs
@@ -8,13 +8,28 @@ namespace GestionDeInventario
 {
     public class Producto
     {
+  
         public string Nombre { get; set; }
         public decimal Precio { get; set; }
+
+    
         public Producto(string nombre, decimal precio)
         {
+            if (string.IsNullOrWhiteSpace(nombre))
+            {
+                throw new ArgumentException("El nombre del producto no puede estar vacío.");
+            }
+
+            if (precio <= 0)
+            {
+                throw new ArgumentException("El precio debe ser un número positivo.");
+            }
+
             Nombre = nombre;
             Precio = precio;
         }
+
+        // Método para mostrar la información del producto
         public void MostrarInformacion()
         {
             Console.WriteLine($"Producto: {Nombre}, Precio: {Precio:C}");
